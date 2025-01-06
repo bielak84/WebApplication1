b65828476c7795c480e3193ee431681a84b5ad94
bielak84 <69897588+bielak84@users.noreply.github.com>
2025-01-06 21:31:11 +0100

    Update ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -3,7 +3,9 @@ namespace WebApplication1.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
+        /// <summary>
+        /// 
+        /// </summary>
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
